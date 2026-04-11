d6716ef698c0a8f83f083d33623610c0b4a16cd4
Gabriel-Villalba <105971696+Gabriel-Villalba@users.noreply.github.com>
2026-04-11 08:11:23 -0300

    redisenio

---
+++ portfolio-redesign/src/data/projects.ts
@@ -0,0 +1,53 @@
+import favStoreImg from '../assets/favStore.jpeg';
+import ahorcadoImg from '../assets/ahorcado.jpeg';
+import raffataImg from '../assets/RaffataImg.jpeg';
+import enBioImg from '../assets/enBio.jpeg';
+
+export interface Project {
+  id: string;
+  title: string;
+  type: string;
+  description: string;
+  image: string;
+  tags: string[];
+  website: string;
+}
+
+export const projects: Project[] = [
+  {
+    id: '1',
+    title: 'FavStore',
+    type: 'Catálogo',
+    description: 'Sitio web para emprendimiento de parrillas portátiles. Muestra productos con imágenes, descripciones detalladas y precios, facilitando la consulta directa por WhatsApp.',
+    image: favStoreImg,
+    tags: ['React','TypeScript', 'TailwindCSS', 'Responsive'],
+    website: 'https://www.favstore.com.ar'
+  },
+   {
+    id: '2',
+    title: 'Ahorcado',
+    type: 'Juego online',
+    description: 'Juego desarrollado como pasatiempo personal. Implementa la lógica clásica del ahorcado, permitiendo a los usuarios adivinar palabras letra por letra mientras se rastrean los intentos y el progreso.',
+    image: ahorcadoImg,
+    tags: ['React', 'TailwindCSS','TypeScript', 'Responsive'],
+    website: 'https://cfc-ahorcado.vercel.app'
+  },
+   {
+    id: '3',
+    title: 'Raffata Pizzas',
+    type: 'Catálogo',
+    description: 'Landing page para Raffata Pizza. Incluye menú dinámico, galería, sección “Sobre nosotros” e integración con WhatsApp. Un proyecto centrado en diseño minimalista y performance, ideal para negocios gastronómicos.',
+    image: raffataImg,
+    tags: ['React', 'TailwindCSS','TypeScript', 'Responsive'],
+    website: 'https://raffata.vercel.app/'
+  },
+   {
+    id: '4',
+    title: 'EnBio',
+    type: 'Landing Page',
+    description: 'Sitio web institucional para bioinsumos agrícolas, destacando soluciones sustentables, productividad y cuidado del suelo.',
+    image: enBioImg,
+    tags: ['HTML5', 'TailwindCSS','JavaScript', 'Responsive'],
+    website: 'https://en-bio.vercel.app'
+  }
+];
